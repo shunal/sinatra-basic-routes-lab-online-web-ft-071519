b3523624fb8fb6b9177a956d08d979dc239a80a0
Steven Hunal <stevehunal@gmail.com>
2019-09-18 01:04:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,16 @@ class App < Sinatra::Base
   get '/' do 
     "Hello, World!"
   end 
+  
   get '/name' do
     "My name is Steve"
   end
+  
+  get '/hometown' do 
+    "My hometown is Fair lawn"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is All Falls Down"
+  end 
 end
